4298978f76bfc552e20a7c74346ec574b0728572
Sualeh Fatehi <sualeh@hotmail.com>
2021-04-01 18:58:01 -0400

    Add coverage for bank card class

--- src/test/java/us/fatehi/test/creditcardnumber/BankCardTest.java
+++ src/test/java/us/fatehi/test/creditcardnumber/BankCardTest.java
@@ -42,4 +42,65 @@ public class BankCardTest {
     assertThat(
         bankCard.toString().replaceAll("\\R", ""), is(bankCardToString.replaceAll("\\R", "")));
   }
+
+  @Test
+  public void bankCard1() throws IOException {
+    final AccountNumber pan = AccountNumbers.accountNumber("5266092201416173");
+    final ExpirationDate expirationDate = new ExpirationDate(2012, 12);
+    final Name name = new Name("Sualeh", "Fatehi");
+
+    final BankCard bankCard = new BankCard(pan, expirationDate, name);
+
+    assertThat(bankCard.hasAccountNumber(), is(true));
+    assertThat(bankCard.getAccountNumber(), is(pan));
+
+    final String bankCardToString =
+        IOUtils.resourceToString("/BankCard1.toString", StandardCharsets.UTF_8);
+    assertThat(
+        bankCard.toString().replaceAll("\\R", ""), is(bankCardToString.replaceAll("\\R", "")));
+  }
+
+  @Test
+  public void bankCard2() throws IOException {
+    final AccountNumber pan = AccountNumbers.accountNumber("5266092201416173");
+    final ExpirationDate expirationDate = new ExpirationDate(2012, 12);
+
+    final BankCard bankCard = new BankCard(pan, expirationDate);
+
+    assertThat(bankCard.hasAccountNumber(), is(true));
+    assertThat(bankCard.getAccountNumber(), is(pan));
+
+    final String bankCardToString =
+        IOUtils.resourceToString("/BankCard2.toString", StandardCharsets.UTF_8);
+    assertThat(
+        bankCard.toString().replaceAll("\\R", ""), is(bankCardToString.replaceAll("\\R", "")));
+  }
+
+  @Test
+  public void bankCard3() throws IOException {
+    final AccountNumber pan = AccountNumbers.accountNumber("5266092201416173");
+
+    final BankCard bankCard = new BankCard(pan);
+
+    assertThat(bankCard.hasAccountNumber(), is(true));
+    assertThat(bankCard.getAccountNumber(), is(pan));
+
+    final String bankCardToString =
+        IOUtils.resourceToString("/BankCard3.toString", StandardCharsets.UTF_8);
+    assertThat(
+        bankCard.toString().replaceAll("\\R", ""), is(bankCardToString.replaceAll("\\R", "")));
+  }
+
+  @Test
+  public void bankCard4() throws IOException {
+
+    final BankCard bankCard = new BankCard();
+
+    assertThat(bankCard.hasAccountNumber(), is(false));
+
+    final String bankCardToString =
+        IOUtils.resourceToString("/BankCard4.toString", StandardCharsets.UTF_8);
+    assertThat(
+        bankCard.toString().replaceAll("\\R", ""), is(bankCardToString.replaceAll("\\R", "")));
+  }
 }
