e003ba5e96ba5cc84215c0fdb52c6fb51df32095
Sualeh Fatehi <sualeh@hotmail.com>
2025-11-23 20:40:10 -0500

    Update copyright year

--- src/main/java/us/fatehi/creditcardnumber/ServiceCode3.java
+++ src/main/java/us/fatehi/creditcardnumber/ServiceCode3.java
@@ -2,7 +2,7 @@
  *
  * Credit Card Number
  * https://github.com/sualeh/credit_card_number
- * Copyright (c) 2014-2025, Sualeh Fatehi.
+ * Copyright (c) 2014-2026, Sualeh Fatehi.
  *
  */
 package us.fatehi.creditcardnumber;
